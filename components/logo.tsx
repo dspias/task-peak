07151c0a0ea3a96157f46fde0f4e28caa12a581b
Pias Das <pdsperrow95@gmail.com>
2024-01-05 16:47:04 +0600

    Add authentication and organization using clerk

---
+++ components/logo.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+import Image from "next/image";
+import Link from "next/link";
+
+const headingFont = localFont({ src: "../public/fonts/font.woff2" });
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
+          {process.env.APP_NAME}
+        </p>
+      </div>
+    </Link>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: process.env.APP_NAME || "",
+  description: process.env.DESCRIPTION || "",
+};
